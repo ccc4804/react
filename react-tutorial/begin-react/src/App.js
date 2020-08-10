522f0d6f0b3231356e2ca56e2286cc269095e4b6
Heewon <ccc4804@naver.com>
2020-08-10 23:46:01 +0900

    Chapter7 - 조건부 렌더링

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Hello from './hello'; //Hello 컴포넌트를 불러온다.
+import Hello from './Hello'; //Hello 컴포넌트를 불러온다.
 import './App.css' //css 불러온다. 확장자명을 적지 않으면 js가 디폴트이다.
 import Wrapper from './Wrapper'
 
@@ -15,7 +15,9 @@ function App() {
   // 아래는 JSX 
   return (
     <Wrapper>
-      <Hello name="react" color="red" /> {/*속성을 넣어 준 것이 props이다.*/}
+      <Hello name="react" color="red" isSpecial={true} /> 
+      {/*isSpecial(모든 옵션 동일)에 아무런 값을 안 넣어주면 true가 default이다.*/}
+       {/*true 또한 js 코드이기 때문에 중괄호로 감싸주어야한다. */}
       <Hello color="pink"></Hello>
     </Wrapper>
   );
--- react-tutorial/begin-react/src/hello.js
+++ react-tutorial/begin-react/src/hello.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // 컴포넌트 이름은 대문자로 시작된다. 카멜 표기법이다.
 /*function Hello(props){
@@ -8,15 +8,22 @@ import React from 'react';
 }*/
 
 // 비구조화 할당 사용
-function Hello({ color, name}){
-    return <div style={{
-        color: color
-    }}>안녕하세요 {name}</div>
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div
+      style={{
+        color: color,
+      }}
+    >
+      {isSpecial && <b>*</b>} {/*옆의 코드와 동일하다. {isSpecial ? <b>*</b> : null}*/}
+      안녕하세요 {name}
+    </div>
+  );
 }
 
 // 특정 값을 빠트렸을 때 default 값을 지정해준다.
 Hello.defaultProps = {
-    name:'이름없음'
-}
+  name: "이름없음",
+};
 
 export default Hello;
