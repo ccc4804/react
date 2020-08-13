b2a6bd598f888666415e4169006ee32ee261de3c
Heewon <ccc4804@naver.com>
2020-08-13 14:24:20 +0900

    Chapter17 - useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기

--- react-tutorial/begin-react/src/UserList.js
+++ react-tutorial/begin-react/src/UserList.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
   //props로 받아온다.
 
   const { username, email, id, active } = user;
+
+  /*
+  useEffect(()=>{
+      console.log(uesr)
+  })*/
+  /*
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+    // props -> state로 작업할 때 사용
+    // REST API를 요청
+    // D3 video.js
+    // setInterval, setTimeout
+    return () => {
+      // clearInterval, clearTimeout
+      // 라이브러리 인스턴스 제거
+      console.log("컴포넌트가 화면에서 사라짐");
+    };
+  }, []);
+  */
+
+  useEffect(() => {
+    console.log("user 값이 설정 됨");
+    console.log(user);
+    // return은 뒷정리 함수라고 보면 된다.
+    return () => {
+      console.log("user 값이 바뀌기 전");
+      console.log(user);
+    };
+  }, [user]);
+  //[user]로 deps를 설정해주면 user의 값이 바뀔 때 마다 호출된다.
 
   return (
     <div>
