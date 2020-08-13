80b0a915cf2b45699d368c2bf8c29bdd59c6271a
Heewon <ccc4804@naver.com>
2020-08-13 09:50:54 +0900

    Chapter13 - useRef로 컴포넌트 안의 변수 만들기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import UserList from './UserList';
+import React, { useRef } from "react";
+import UserList from "./UserList";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: "hee",
+      email: "test1@test.com",
+    },
+    {
+      id: 2,
+      username: "yoon",
+      email: "test2@test.com",
+    },
+    {
+      id: 3,
+      username: "hw",
+      email: "test3@test.com",
+    },
+  ];
 
-  // 아래는 JSX 
-  return (
-    <UserList />
-  );
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current);
+    nextId.current += 1;
+  }
+
+  // 아래는 JSX
+  return <UserList users={users} />;
 }
 
 export default App;
--- react-tutorial/begin-react/src/UserList.js
+++ react-tutorial/begin-react/src/UserList.js
@@ -9,24 +9,7 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "hee",
-      email: "test1@test.com",
-    },
-    {
-      id: 2,
-      username: "yoon",
-      email: "test2@test.com",
-    },
-    {
-      id: 3,
-      username: "hw",
-      email: "test3@test.com",
-    },
-  ];
+function UserList({ users }) {
   return (
     /*
     <div>
@@ -38,7 +21,7 @@ function UserList() {
     // 배열의 길이가 고정적이지 않을 때는 아래와 같이 map을 이용하여 배열을 출력한다.
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id}/> 
+        <User user={user} key={user.id} />
         //key가 있으면 key를 설정해준다.
         //key가 없으면 key={index}로 설정해주는데 이는 매우 비효율적인 구조이다.
         //index는 배열의 순서만 의미하므로 배열의 구조상 삽입, 변경, 삭제 시 비효율적이기 때문이다.
