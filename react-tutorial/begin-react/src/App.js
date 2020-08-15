ba51424c92d60807124546d07d9c9ec1de61dca9
Heewon <ccc4804@naver.com>
2020-08-16 00:03:28 +0900

    Chapter31 - componentDidCatch 메서드 - 에러처리 시 브라우저에 경고창을 띄우는 방법

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,91 +1,16 @@
-import React, { useReducer, useMemo, createContext } from "react";
-import UserList from "./UserList";
-import CreateUser from "./CreateUser";
-import produce from 'immer';
-
-function countActiveUsers(users) {
-  console.log("활성 사용자 수를 세는 중...");
-  return users.filter((user) => user.active).length;
-}
-
-// App 컴포넌트의 설정 값을 외부로 뺀다.
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: "hee",
-      email: "test1@test.com",
-      active: true,
-    },
-    {
-      id: 2,
-      username: "yoon",
-      email: "test2@test.com",
-      active: false,
-    },
-    {
-      id: 3,
-      username: "hw",
-      email: "test3@test.com",
-      active: false,
-    },
-  ],
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "CREATE_USER":
-      return produce(state, draft => {
-        draft.users.push(action.user);
-      })
-
-      /*
-      return {}
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),        
-      };*/
-    case "TOGGLE_USER":
-      return produce(state, draft => {
-        const user = draft.users.find(user => user.id === action.id);
-        user.active = !user.active;
-      })  
-    /*return {
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };*/
-    case "REMOVE_USER":      
-      return produce(state, draft => { 
-        // splice를 사용하기 위해서는 index를 알아야하므로 해당 id를 이용하여 index 값을 가져온다. 
-        const index = draft.users.findIndex(user => user.id === action.id)
-        draft.user.splice(index,1)
-      });
-    /*return {
-        users: state.users.filter((user) => user.id !== action.id),
-      };*/
-    default:
-      throw new Error("Unhandled action");
-  }
-}
-
-// 외부의 컴포넌트에서 사용 가능하게 context를 export 해준다.
-export const UserDispatch = createContext(null);
+import React from "react";
+import User from "./Chapter31/User";
+import ErrorBoundary from "./Chapter31/ErrorBoundary";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { users } = state;
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
-  // 아래는 JSX
+  const user = {
+    id: 1,
+    username: "hee",
+  };
   return (
-    // UserDispatch Context의 value에 dispatch를 지정해준다.
-    <UserDispatch.Provider value = {dispatch}>
-      <CreateUser />
-      <UserList users={users} />
-      <div>활성 사용자 수 : {count}</div>
-    </UserDispatch.Provider>
+    <ErrorBoundary>
+      <User />
+    </ErrorBoundary>
   );
 }
 
--- react-tutorial/begin-react/src/index.js
+++ react-tutorial/begin-react/src/index.js
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import LifeCycleSample from "./LifeCycleSample";
+// DOM에서 id가 root인 애를 찾아서 react App을 넣겠다.
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// 랜덤 색상을 생성합니다
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-function App() {
-  const [color, setColor] = useState("#000000");
-  const [visible, setVisible] = useState(true);
-
-  const onClick = () => {
-    setColor(getRandomColor());
-  };
-
-  const onToggle = () => {
-    setVisible(!visible);
-  };
-
-  return (
-    <>
-      <button onClick={onClick}>랜덤 색상</button>
-      <button onClick={onToggle}>토글</button>
-      {visible && <LifeCycleSample color={color} />}
-    </>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
