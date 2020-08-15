90c31f116a725d73a9c6d5fb2dfeba7efef83be3
Heewon <ccc4804@naver.com>
2020-08-16 02:11:46 +0900

    Chapter(33-36) - Prettier, ESLint, Snippet 참고 : https://react.vlpt.us/basic/27-useful-tools.html

--- react-tutorial/begin-react/src/ContextSample.js
+++ react-tutorial/begin-react/src/ContextSample.js
@@ -7,27 +7,25 @@ const MyContext = createContext("defaultValue");
 function Child() {
   //useContext : 해당 Context의 value를 불러온다.
   const text = useContext(MyContext);
-  return <div>안녕하세요? {text}</div>
+  return <div>안녕하세요? {text}</div>;
 }
 
 function Parent({ text }) {
-  return <Child text={text} />
+  return <Child text={text} />;
 }
 
 function GrandParent({ text }) {
-  return <Parent text={text} />
+  return <Parent text={text} />;
 }
 
 function ContextSample({ text }) {
-    const [value, setValue] = useState(true)
-   
+  const [value, setValue] = useState(true);
+
   return (
     // Context.Provider : Context 제공자인데 value 값을 입력하면 해당 Context의 value가 변경된다.
-    <MyContext.Provider value={value ? 'GOOD' : 'BAD'}>
+    <MyContext.Provider value={value ? "GOOD" : "BAD"}>
       <GrandParent text="GOOD2" />
-      <button onClick={()=>
-      setValue(!value)
-    }>Click Me!</button>
+      <button onClick={() => setValue(!value)}>Click Me!</button>
     </MyContext.Provider>
   );
 }
