e50bcb4b79b9e08b04f686125b466299416edd25
Heewon <ccc4804@naver.com>
2020-08-16 00:39:20 +0900

    Chapter32 - componentDidCatch와 SENTRY 연동
    주소 : https://sentry.io/
    참고 : https://react.vlpt.us/basic/26-componentDidCatch-and-sentry.html

--- react-tutorial/begin-react/src/index.js
+++ react-tutorial/begin-react/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn:
+    "https://30406c8ad8424a8e93822989cf48ccb0@o434531.ingest.sentry.io/5391711",
+});
 
 // DOM에서 id가 root인 애를 찾아서 react App을 넣겠다.
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
