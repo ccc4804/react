13014269b671fc4e0e8eb6cae4fe9aba0986c91d
Heewon <ccc4804@naver.com>
2020-08-15 15:43:25 +0900

    Chapter27 - reduce immer로 구현하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -3,9 +3,6 @@ import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import produce from 'immer';
 
-// 크롬의 콘솔창에서 immer (produce)를 사용할 수 있게 설정함
-window.produce =  produce
-
 function countActiveUsers(users) {
   console.log("활성 사용자 수를 세는 중...");
   return users.filter((user) => user.active).length;
@@ -38,22 +35,34 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      })
+
+      /*
+      return {}
         inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+        users: state.users.concat(action.user),        
+      };*/
     case "TOGGLE_USER":
-      return {
-        ...state,
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })  
+    /*return {
         users: state.users.map((user) =>
           user.id === action.id ? { ...user, active: !user.active } : user
         ),
-      };
-    case "REMOVE_USER":
-      return {
-        ...state,
+      };*/
+    case "REMOVE_USER":      
+      return produce(state, draft => { 
+        // splice를 사용하기 위해서는 index를 알아야하므로 해당 id를 이용하여 index 값을 가져온다. 
+        const index = draft.users.findIndex(user => user.id === action.id)
+        draft.user.splice(index,1)
+      });
+    /*return {
         users: state.users.filter((user) => user.id !== action.id),
-      };
+      };*/
     default:
       throw new Error("Unhandled action");
   }
